dbc4d63bef1dcdb231534412fee9d72b84faac92
Simon Vass <svass@etech.ug>
2014-01-13 00:19:35 +0200

    Inital Commit

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -6,6 +6,7 @@ validation_client_name   'chef-validator'
 validation_key           '/home/svass/chef-repo/.chef/chef-validator.pem'
 chef_server_url          'https://41.190.141.16:443'
 syntax_check_cache_path  '/home/svass/chef-repo/.chef/syntax_check_cache'
+cookbook_path 		 '/home/svass/chef-repo/cookbooks'
 knife[:pve_cluster_url] = "#{ENV['PVE_CLUSTER_URL']}"
 knife[:pve_user_name]   = "#{ENV['PVE_USER_NAME']}"
 knife[:pve_user_password] = "#{ENV['PVE_PASSWORD_NAME']}"
